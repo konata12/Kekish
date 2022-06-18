ae7b99fbe10beaf8b3015d5797171bd5a0eb3787
Dima Kekish <dimakeshaa@gmail.com>
2022-06-18 23:04:53 +0300

    lesson-11 finished

---
+++ Homework/lesson-11/assets/js/modal.js
@@ -0,0 +1,58 @@
+// console.log(4)
+// // modal-open-button_click
+// let item_1 = document.querySelector('#item-1')
+// item_1.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-1').classList.add('modal-active')
+// })
+
+// let item_2 = document.querySelector('#item-2')
+// item_2.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-2').classList.add('modal-active')
+// })
+
+// let item_3 = document.querySelector('#item-3')
+// item_3.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-3').classList.add('modal-active')
+// })
+
+
+// // modal-open-img_click
+// let item_1_1 = document.querySelector('#item-1_1')
+// item_1_1.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-1').classList.add('modal-active')
+// })
+
+// let item_2_1 = document.querySelector('#item-2_1')
+// item_2_1.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-2').classList.add('modal-active')
+// })
+
+// let item_3_1 = document.querySelector('#item-3_1')
+// item_3_1.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-3').classList.add('modal-active')
+// })
+
+// // modal-close
+// let close_1 = document.querySelector('#close-1')
+// close_1.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-1').classList.remove('modal-active')
+// })
+
+// let close_2 = document.querySelector('#close-2')
+// close_2.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-2').classList.remove('modal-active')
+// })
+
+// let close_3 = document.querySelector('#close-3')
+// close_3.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('#modal-3').classList.remove('modal-active')
+// })
