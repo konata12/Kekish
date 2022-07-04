018aa34672d7496293320099f15ea3478d59b501
Dima Kekish <dimakeshaa@gmail.com>
2022-07-04 18:18:55 +0300

    lesson-17 progres

---
+++ Homework/lesson-17/assets/js/scirpts.js
@@ -0,0 +1,38 @@
+jq2 = jQuery.noConflict();
+jq2(function( $ ) {
+    $('.owl-carousel-1').owlCarousel({
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        loop:true,
+        margin:5,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1500:{
+                items:4
+            }
+        }
+    })
+    $('.owl-carousel-2').owlCarousel({
+        autoplay:false,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        loop:true,
+        dots:true,
+        dotsEach:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+});
