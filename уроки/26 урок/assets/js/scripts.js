73c59e045c32991a88700f6918a60ca5459e6d53
Dima Kekish <dimakeshaa@gmail.com>
2022-07-26 23:09:43 +0300

    lesson-26

---
+++ уроки/26 урок/assets/js/scripts.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b = 2;
+const c = 5;
+
+const res = a + b + c;
+
+document.getElementById('app').innerHTML = `<h1> ${res} </h1>`;
+
+const name = prompt('What\'s your name');
+alert(`Your name is ${name}`);
