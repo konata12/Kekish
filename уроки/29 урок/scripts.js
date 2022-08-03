508a917f769f3597cfea7769610c9c32f154c940
Dima Kekish <dimakeshaa@gmail.com>
2022-08-03 20:40:07 +0300

    lesson 29 lab

---
+++ уроки/29 урок/scripts.js
@@ -0,0 +1,27 @@
+const color = prompt("Choose your color", 'white')
+
+switch (color.toLowerCase()) {
+    case 'red':
+        console.log('You choose right color')
+    break;
+
+    case 'yellow':
+        console.log('You choose right color')
+    break;
+
+    case 'green':
+        console.log('You choose right color')
+    break;
+
+    case 'purle':
+        console.log('You choose right color')
+    break;
+
+    case 'brown':
+        console.log('You choose right color')
+    break;
+
+    default:
+        console.log('You choosed wrong color')
+    break;
+}
