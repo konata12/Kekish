843fee97df43bf977bed87d37d9af557e3e2fb97
Dima Kekish <dimakeshaa@gmail.com>
2022-08-05 20:36:25 +0300

    new animations and finishes resub animation

---
+++ Набиваю руку/Animation – raids/assets/js/scripts.js
@@ -0,0 +1,16 @@
+const letter = document.getElementsByClassName('letter')
+console.dir(letter)
+
+for (key of letter) {
+    if ( key.innerHTML === 'e') {
+        key.classList.add('e-width')
+        console.log(key.clientWidth)
+    }
+}
+
+for (key of letter) {
+    if ( key.innerHTML === 'c') {
+        key.classList.add('c-width')
+        console.log(key.clientWidth)
+    }
+}
