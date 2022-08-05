7405d8dfa10872f1516cd003fdfd0902795adf86
Dima Kekish <dimakeshaa@gmail.com>
2022-08-05 21:08:36 +0300

    lesson-27

---
+++ Homework/lesson-27/assets/js/scripts.js
@@ -0,0 +1,10 @@
+const names = [
+    'Biba', 'Boba', 'Ne Biba', 'Ne boba', 'Bob',
+    'Ne Bob', 'Maks', 'Ne Maks', 'Ragnar', 'Ne ragnar'
+]
+
+for ( let i = 0; i <= names.length; i++ ) {
+    if ( names[i] === 'Ragnar' ) {
+        console.log( names[i] + ' Lodbrok')
+    }
+}
---
+++ Homework/lesson-29/assets/js/scripts.js
@@ -0,0 +1,14 @@
+const str_1 = prompt('Your name')
+document.getElementById('name').innerHTML = `<h2> ${str_1} </h2>`
+
+const str_2 = prompt('Your surname')
+document.getElementById('surname').innerHTML = `<h2> ${str_2} </h2>`
+
+const str_3 = prompt('Your age')
+document.getElementById('age').innerHTML = `<h2> ${str_3} </h2>`
+
+const str_4 = prompt('Your city')
+document.getElementById('city').innerHTML = `<h2> ${str_4} </h2>`
+
+const str_5 = prompt('Your job')
+document.getElementById('job').innerHTML = `<h2> ${str_5} </h2>`
